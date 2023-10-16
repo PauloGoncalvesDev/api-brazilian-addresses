622861bc95eb0e54bf1eb5346de01b2ebd6a39da
Paulo Guilherme <paulinnho_testes_csharp@hotmail.com>
2023-10-16 17:53:55 -0300

    TASK3 - Cria regra de negócio para IBGE request

--- src/Backend/BrazilianAddresses.Application/BusinessRules/IBGEBusinessRule/CreateIBGE.cs
+++ src/Backend/BrazilianAddresses.Application/BusinessRules/IBGEBusinessRule/CreateIBGE.cs
@@ -1,5 +1,9 @@
-﻿using BrazilianAddresses.Application.Validators.IBGEValidator;
+﻿using AutoMapper;
+using BrazilianAddresses.Application.Validators.IBGEValidator;
 using BrazilianAddresses.Communication.Requests;
+using BrazilianAddresses.Domain.Entities;
+using BrazilianAddresses.Domain.Repositories;
+using BrazilianAddresses.Domain.Repositories.IBGERepository;
 using BrazilianAddresses.Exceptions.ExceptionsBase;
 using FluentValidation.Results;
 
@@ -7,9 +11,28 @@ namespace BrazilianAddresses.Application.BusinessRules.IBGEBusinessRule
 {
     public class CreateIBGE : ICreateIBGE
     {
+        private readonly IIBGEWriteOnlyRepository _ibgeWriteOnlyRepository;
+
+        private readonly Mapper _mapper;
+
+        private readonly IWorkUnit _workUnit;
+
+        public CreateIBGE(IIBGEWriteOnlyRepository ibgeWriteOnlyRepository, Mapper mapper, IWorkUnit workUnit)
+        {
+            _ibgeWriteOnlyRepository = ibgeWriteOnlyRepository;
+            _mapper = mapper;
+            _workUnit = workUnit;
+        }
+
         public async Task Execute(IBGERequestJson ibgeRequestJson)
         {
             ValidateIBGE(ibgeRequestJson);
+
+            IBGE ibge = _mapper.Map<IBGE>(ibgeRequestJson);
+
+            await _ibgeWriteOnlyRepository.Add(ibge);
+
+            await _workUnit.Commit();
         }
 
         private static void ValidateIBGE(IBGERequestJson ibgeRequestJson)
