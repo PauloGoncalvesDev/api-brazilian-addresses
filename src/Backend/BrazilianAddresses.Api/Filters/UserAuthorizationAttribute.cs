fabc52740b076e30eade5dc1e65b309496b92b21
Paulo Guilherme <paulinnho_testes_csharp@hotmail.com>
2023-10-22 09:18:39 -0300

    TASK13 feat: adiciona arquivo UserAuthorizationAttribute

---
+++ src/Backend/BrazilianAddresses.Api/Filters/UserAuthorizationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using BrazilianAddresses.Application.Services.Token;
+using BrazilianAddresses.Domain.Repositories.UserRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BrazilianAddresses.Api.Filters
+{
+    public class UserAuthorizationAttribute : AuthorizeAttribute, IAsyncAuthorizationFilter
+    {
+        private readonly TokenController _tokenController;
+
+        private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+
+        public UserAuthorizationAttribute(TokenController tokenController, IUserReadOnlyRepository userReadOnlyRepository)
+        {
+            _tokenController = tokenController;
+            _userReadOnlyRepository = userReadOnlyRepository;
+        }
+
+        public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
