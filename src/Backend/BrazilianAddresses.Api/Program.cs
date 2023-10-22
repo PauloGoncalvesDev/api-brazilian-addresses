9ec2d71e651a01f8108f96a960d207f3b26fa9fc
Paulo Guilherme <paulinnho_testes_csharp@hotmail.com>
2023-10-22 17:01:15 -0300

    BG14 feat: adiciona politica de cors

--- src/Backend/BrazilianAddresses.Api/Program.cs
+++ src/Backend/BrazilianAddresses.Api/Program.cs
@@ -43,6 +43,14 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: "SwaggerPolicy", policy =>
+    {
+        policy.WithOrigins("https://editor.swagger.io/").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 builder.Services.AddRepository(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilters)));
@@ -69,6 +77,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("SwaggerPolicy");
 
 UpdateDatabase();
 
